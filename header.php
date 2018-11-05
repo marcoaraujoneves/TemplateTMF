84945025aa3699aa165493ddea57722a3f863d5a
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2018-11-05 19:34:06 -0200

    Adição de arquivos no menu

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    </head>
+
+    <body>
+        <div class="bg">
+            <div class="container">
+                <div class="row">
+                <div class="col-sm-4">
+                    <p class="textomenu">(22)9999-8756<br><br>07h até as 17h</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="img/logo.png" id="logo">
+                </div>
+                <div class="col-sm-4">       
+                    <p class="textomenu">contato@gmail.com</p>
+                    <a target="_blank" href="#"><img class="social_icone" src="img/fb.png"></a>
+                    <a target="_blank" href="#"><img class="social_icone" src="img/ig.png"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Botao Toggle -->
+                    <button type="button" class="navbar-toggler custom-toggler" data-toggle="collapse" data-target="#menu-navegacao" aria-expanded="true" aria-label="Alternar Navegação">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="menu-navegacao">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Produtos</a></li>
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Serviços</a></li>
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Portifólio</a></li>
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Sobre nós</a></li>
+                        <li class="nav-item navegacao"><a class="nav-link" href="#">Contatos</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+
+    </body>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.js"></script>
+</html>
