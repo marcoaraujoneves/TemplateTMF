0fea80d4b2e60c78eff0ff4ef8794a2c9cb1972f
breinoso <breinoso2006@gmail.com>
2018-11-26 23:14:00 -0200

    Recriação do rodapé

---
+++ footer.php
@@ -0,0 +1,67 @@
+<section id="rodape">
+    <!-- Títulos -->  
+    <div class="row">
+        <div class="col-md-1">
+            <!--espaçamento--> 
+        </div>
+        <div class="col-md-3">
+            <h1 class="titulos_rodape">Menu</h1>
+        </div>
+        <div class="col-md-4">
+            <h1 class="titulos_rodape">Funcionamento</h1>
+        </div>
+        <div class="col-md-3">
+            <h1 class="titulos_rodape">Principais Tags</h1>
+        </div>
+        <div class="col-md-1">
+            <!--espaçamento--> 
+        </div>
+    </div>
+    <!-- Menus --> 
+    <div class="row">
+        <div class="col-md-1">
+            <!--espaçamento--> 
+        </div>
+        <div class="col-md-3">
+            <ul>
+                <li><a class="item_menu_rodape">> Início </a></li>
+                <li><a class="item_menu_rodape">> Sobre Nós </a></li>
+                <li><a class="item_menu_rodape">> Produtos </a></li>
+                <li><a class="item_menu_rodape">> Serviços </a></li>
+                <li><a class="item_menu_rodape">> Portfólios </a></li>
+                <li><a class="item_menu_rodape">> Contato </a></li>
+            </ul>
+        </div>
+        <div class="col-md-4">
+            <ul> 
+                <li class="item_funcionamento"> <span class="pre_item_funcionamento">Local:</span> Rua Prudente De morais, 200 - Lot. São João, Nova Friburgo - RJ </li>
+                <li class="item_funcionamento"> <span class="pre_item_funcionamento">Número para contato:</span> (22) 2523-2525 </li>
+                <li class="item_funcionamento"> <span class="pre_item_funcionamento">Email para contato:</span> contato@tmf.com.br </li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <span class="tags">Tornearia</span> 
+            <span class="tags">Projeto de Máquinas</span> 
+            <span class="tags">Usinagem</span>
+            <span class="tags">Manutenção Preventiva</span>
+        </div>
+        <div class="col-md-1">
+            <!--espaçamento--> 
+        </div>
+    </div>
+    <!-- Direitos --> 
+    <div class="row">
+        <div class="col-md-2">
+            <!--espaçamento--> 
+        </div> 
+        <div class="col-md-6"> 
+            <span id="texto_direitos">©2018. Todos os direitos reservados.</span>
+        </div>
+        <div class="col-md-2"> 
+            <!-- Inserir logo aqui -->
+        </div>
+        <div class="com-md-2"> 
+            <!--espaçamento--> 
+        </div>
+    </div>
+</section>
