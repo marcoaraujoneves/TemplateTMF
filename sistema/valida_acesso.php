43f19216e76f4ca80003dc20c49c8bd02da2610f
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-01-29 10:27:34 -0200

    Adição de métodos bind_param ao PHP para prevenir sql injection

--- sistema/valida_acesso.php
+++ sistema/valida_acesso.php
@@ -1,35 +1,5 @@
 <?php
-    session_start();
-    include ('db.class.php');
-    $login = $_POST['login'];
-    $senha = md5($_POST['senha']);
-    $objDB = new db();
-    $link = $objDB->conecta_mysql();
-    $sql = "SELECT * FROM usuario WHERE login='$login' AND senha='$senha'";
-    $resultado_id = mysqli_query($link, $sql);
-    
-    if($resultado_id){
-        $dados_usuario = mysqli_fetch_array($resultado_id);
-        
-        if(isset($dados_usuario['login'])){
-            $_SESSION['nome'] = $dados_usuario['nome'];
-            $_SESSION['cargo'] = $dados_usuario['cargo'];
-            header('Location: gerenciador.php');
-        }else{
-            header('Location: index.php?erro=1');
-        }
-    }else{
-        echo 'Erro ao buscar o usuário no Banco de Dados';
-    }
-?>
 
-
-
-
-
-
-<?php
-/*
     session_start();
 
     include ('db.class.php');
@@ -44,23 +14,20 @@
     $stmt = $link->prepare($sql);
     $stmt->bind_param('ss',$login,$senha);
     $stmt->execute();
-
-    $resultado_id = $stmt->store_result();
-    var_dump($resultado_id);
-    //$resultado_id = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-//    if($resultado_id){
-//        $dados_usuario = mysqli_fetch_array($resultado_id);
+    $resultado_id = $stmt->get_result();
+    
+    if($resultado_id->num_rows){
+        $resultado_id = $resultado_id->fetch_assoc();
         
-//        if(isset($dados_usuario['login'])){
-//            $_SESSION['nome'] = $dados_usuario['nome'];
-//            $_SESSION['cargo'] = $dados_usuario['cargo'];
-//            header('Location: gerenciador.php');
-//        }else{
-//            header('Location: index.php?erro=1');
-//        }
-//    }else{
-//        echo 'Erro ao buscar o usuário no Banco de Dados';
-//    }
-*/
+        if(isset($resultado_id['login']) && isset($resultado_id['cargo'])) {
+            $_SESSION['nome'] = $resultado_id['nome'];
+            $_SESSION['cargo'] = $resultado_id['cargo'];
+            header('Location: gerenciador.php');
+        }else{
+            //header('Location: index.php?erro=1');
+        }
+    }else{
+        header('Location: index.php?erro=1');
+    }
+
 ?>
