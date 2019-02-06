f54d7a3931c4cf513a7e8c4af0fea58aa1b013ca
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2019-02-06 19:33:50 -0200

    Adição de requisição AJAX para trocar entre os módulos do sistema no painel administrativo.

---
+++ sistema/mod_produtos.php
@@ -0,0 +1,7 @@
+<h1 class="titulo_secoes"> Produtos </h1>
+<p>
+    <br>
+    A tabela abaixo contém todos os produtos cadastrados no sistema! 
+    Para Inserir, Deletar ou Editar basta clicar no respectivo botão e inserir as informações necessárias 
+    para cada procedimento.
+</p>
