3a71c8a1b341cb8cfdb1e5194474bea3f7dc24dd
Bruno Reinoso <40645913+Breinoso2006@users.noreply.github.com>
2019-02-16 17:27:46 -0200

    Add files via upload
    Pag Produtos

--- produtos.php
+++ produtos.php
@@ -17,7 +17,8 @@
         <link rel="stylesheet" href="css/footer.css">
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-
+        <link rel="stylesheet" href="css/main.css">
+        
         <title> Indústria TMF | Home </title> 
 
     </head>
@@ -31,6 +32,100 @@
 
         <!-- Corpo principal -->
         <main>
+        <section id="secaoProdutos" class="secaoSite" style="margin-top:140px;" >
+        <div class="container container-fluid">
+                <div class="row">
+                    <div class="col-md-12 container-titulos">
+                        <h1 class="titulo_secoes">
+                            Produtos
+                        </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                        <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-5 my-auto mx-auto"> 
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <img src="img/tornearia.png" alt="Avatar" class="img_prod2">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <img src="img/tornearia.png" alt="Avatar" class="img_prod2">
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <img src="img/usinagem.png" alt="Avatar" class="img_prod2">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <img src="img/usinagem.png" alt="Avatar" class="img_prod2">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Nome Produto</h4>
+                                <p class="card-text" style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi. Vitae et leo duis ut diam quam nulla. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Eget duis at tellus at urna condimentum mattis pellentesque id. Blandit volutpat maecenas volutpat blandit aliquam. Augue ut lectus arcu bibendum at. Vehicula ipsum a arcu cursus vitae congue mauris. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Vitae ultricies leo integer malesuada nunc. Eu volutpat odio facilisis mauris sit amet. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit.
+
+                                    Morbi tincidunt augue interdum velit. At in tellus integer feugiat scelerisque varius morbi. Porttitor massa id neque aliquam. Pellentesque massa placerat duis ultricies lacus sed. Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. Mattis nunc sed blandit libero volutpat sed cras ornare. Sem viverra aliquet eget sit amet tellus cras adipiscing. Scelerisque in dictum non consectetur a. Rhoncus mattis rhoncus urna neque. Adipiscing commodo elit at imperdiet. Placerat vestibulum lectus mauris ultrices eros in. Orci sagittis eu volutpat odio facilisis. Est ultricies integer quis auctor. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Erat nam at lectus urna duis convallis convallis tellus. Tincidunt eget nullam non nisi est sit. Mauris nunc congue nisi vitae suscipit tellus mauris. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Urna et pharetra pharetra massa massa ultricies.
+
+                                     Faucibus scelerisque eleifend donec pretium. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet. Pretium nibh ipsum consequat nisl vel pretium lectus quam. Placerat orci nulla pellentesque dignissim. Et magnis dis parturient montes. Eu lobortis elementum nibh tellus molestie nunc non blandit. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Auctor eu augue ut lectus arcu. Elementum integer enim neque volutpat ac tincidunt vitae. Ut eu sem integer vitae justo.</p>
+                                <input type="submit" class="btn btn-primary" data-toggle="modal" data-target="#ModalProd" id="enviar" name="contratar" value="Contratar">
+                                <!-- Modal -->
+                                <div class="modal fade" id="ModalProd" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Contato</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                            <form>
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <label class="txtForm" for="nome"> &ensp;Nome </label> <br>
+                                                    <input type="text" class="inpForm" id="nome" name="nome" autocomplete="off" required>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label class="txtForm" for="telefone"> &ensp;Telefone </label> <br>
+                                                    <input type="text" class="inpForm" id="telefone" name="telefone" autocomplete="off" required>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label class="txtForm" for="email"> &ensp;E-mail </label> <br>
+                                                    <input type="email" class="inpForm" id="email" name="email" autocomplete="off" required>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label class="txtForm" for="mensagem"> &ensp;Mensagem </label> <br>
+                                                    <textarea class="inpForm" id="mensagem" name="mensagem" rows="6" required> </textarea>
+                                                </div>
+                                            </div>
+                                            <br>
+                                        </div>
+                                    </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="enviar" value="Enviar">  
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
             
         </main>
         <!-- Inclusão do rodapé do site -->
@@ -38,6 +133,7 @@
             <?php include_once('footer.php'); ?>
         </footer>
     </body>
+    
     <script src="js/jquery-3.3.1.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
