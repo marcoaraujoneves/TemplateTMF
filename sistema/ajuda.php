0b4bd0295a9d7eaecc7cecc27e295e1092266385
ProjetosSerraJr <projetoscopmutacao@serrajr.eng.br>
2019-03-21 17:08:45 -0300

    Inicio da parte ajuda do sistema

---
+++ sistema/ajuda.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/responsividade.css">
+
+        <title> Indústria TMF | Home </title> 
+
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-md navbar-light" id="navbarTMF">
+        <div class="container">
+            <img src="../img/logoTMFBranco.png" class="mx-auto" id="logo_Menu">
+        </div>
+    </nav>
+
+    <section id="secaoServicos" class="secaoSite" style="margin-top:30px" >
+        <div class="container container-fluid">
+                <div class="row">
+                    <div class="col-md-12 container-titulos">
+                        <h1 class="titulo_secoes">
+                            Tópicos Principais
+                        </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser" style="witdh:50%;height=50%">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Serviços</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser" style="witdh:50%;height=50%">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Produtos</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Potifólio</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser" style="witdh:50%;height=50%">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Mensagens</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser" style="witdh:50%;height=50%">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Textos</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4"> 
+                        <div class="card" style="width: 100%; height: auto; margin: 50px;background-color: rgba(109, 109, 109, 0.096);">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6 my-auto"> 
+                                        <img src="../img/prod.png" alt="Avatar" class="img_ser" style="witdh:50%;height=50%">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4 style="font-size: 27px;font-weight: 600;color: rgb(41, 70, 117);">Newsletter</h4>
+                                        <p class="card-text" style="text-align: justify;">Adicionar</p>
+                                        <p class="card-text" style="text-align: justify;">Remover</p>
+                                            
+                                        <input type="submit" class="btn btn-primary" id="enviar" name="SaibaMais" value="Saiba Mais">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+        
+
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6"> 
+                        <span id="texto_direitos">©2018. Todos os direitos reservados.</span>
+                    </div>
+                    <div class="col-md-6"> 
+                        <img src="../img/logo1.png" id="logo">
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+
+
+</html>
