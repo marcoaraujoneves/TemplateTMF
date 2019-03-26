64a83e8245ca46bdad9eb25e49219e0b3eafb2b2
Breinoso2006 <breinoso2006@gmail.com>
2019-03-26 01:41:18 -0300

    Perguntas e respostas da área de ajuda do sistema

---
+++ sistema/perguntasajuda.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+        <head>
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                <!--[if lt IE 9]>
+                <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+                <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                <![endif]-->
+                
+                <link rel="stylesheet" href="../css/Sistema.css">
+                <link rel="stylesheet" href="../js/jquery-3.3.1.js">
+                <link rel="stylesheet" href="../css/perguntasajuda.css">
+                <link rel="stylesheet" href="../css/normalize.css">
+                <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+                <link rel="stylesheet" href="../css/responsividade.css">
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+                <title> Sistema de Gerenciamento | TMF </title>
+
+        </head>
+
+        <body>          
+                <div id="accordion">
+                        <div class="card">
+                                <div class="card-header" id="headingOne">
+                                        <h5 class="mb-0">
+                                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                        Como adicionar um serviço?
+                                                </button>
+                                        </h5>
+                                </div>
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                        <div class="card-body">
+                                                Para adicionar um serviço, siga os seguintes passos: <html> <br> </html>
+                                                        1. Vá até o gerenciador do sistema e clique em "Serviço"; <html> <br> </html>
+                                                        2. Digite o Nome e a Descrição para seu novo Serviço em suas respectivas caixas; <html> <br> </html>
+                                                        3. Adicione uma imagem para representar esse novo Serviço; <html> <br> </html>
+                                                        4. Clique no botão "Enviar";
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="card">
+                                <div class="card-header" id="headingTwo">
+                                        <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                                        Como remover um serviço?
+                                                </button>
+                                        </h5>
+                                </div>
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                        <div class="card-body">
+                                                Para remover um serviço, siga os seguintes passos: <html> <br> </html>
+                                                        1. Vá até o gerenciador do sistema e clique em "Serviço"; <html> <br> </html>
+                                                        2. Na parte inferior da página, haverá uma lista dos serviços já cadastrados; <html> <br> </html>
+                                                        3. Encontre o serviço que gostaria de excluir; <html> <br> </html> 
+                                                        4. Clique no botão "Excluir"; <html> <br> </html>
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="card">
+                                <div class="card-header" id="headingThree">
+                                        <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                        Como alterar a descrição de um serviço?
+                                                </button>
+                                        </h5>
+                                </div>
+                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                        <div class="card-body">
+                                                Explicação o mais simples possível de como se altera a descrição de um serviço
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="card">
+                                <div class="card-header" id="headingFour">
+                                        <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                                        Possível dúvida
+                                                </button>
+                                        </h5>
+                                </div>
+                                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                                        <div class="card-body">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                        </div>
+                                </div>
+                        </div>
+
+                        <div class="card">
+                                <div class="card-header" id="headingFive">
+                                        <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                                        Possível dúvida
+                                                </button>
+                                        </h5>
+                                </div>
+                                <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                                        <div class="card-body">
+                                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                        </div>
+                                </div>
+                        </div>                  
+                </div>
+               
+        </body>
+
+</html>
