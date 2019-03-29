8957ba80077dbfab0281c834fbbf65469cc0bd7b
Breinoso2006 <breinoso2006@gmail.com>
2019-03-28 21:09:05 -0300

    Correção no código de perguntasajuda.php

--- sistema/perguntasajuda.php
+++ sistema/perguntasajuda.php
@@ -12,14 +12,10 @@
             <![endif]-->
             
             <link rel="stylesheet" href="../css/Sistema.css">
-            <link rel="stylesheet" href="../js/jquery-3.3.1.js">
             <link rel="stylesheet" href="../css/perguntasajuda.css">
             <link rel="stylesheet" href="../css/normalize.css">
             <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
             <link rel="stylesheet" href="../css/responsividade.css">
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
             <title> Sistema de Gerenciamento | TMF </title>
 
@@ -205,5 +201,8 @@
             </div>
             
         </body>
-
+        <script src="../js/jquery-3.3.1.js"> <script>
+        <script src="../js/popper.min.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+        <script src="../js/main.js"></script>
 </html>
