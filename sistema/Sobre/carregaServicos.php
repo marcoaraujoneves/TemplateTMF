6625fd071af3b7c0634b86cde7754475d4b948ba
ProjetoSerraJr <projetoscomputacao@serrajr.eng.br>
2019-05-02 14:02:39 -0300

    Continuação da implementação da integração da Seção Serviços

--- sistema/Sobre/carregaServicos.php
+++ sistema/Sobre/carregaServicos.php
@@ -5,15 +5,24 @@
     $objDB = new db();
     $link = $objDB->conecta_mysql();
 
-    echo $_GET['ser1'].'<br>'.$_GET['ser2'].'<br>'.$_GET['ser3'].'<br>'.$_GET['ser4'];
-    die();
-    $sql="SELECT * FROM sobre";
+    $sql="SELECT S.nome AS nomeservico,I.nome AS imgservico
+          FROM servico S INNER JOIN imagemservico I
+          ON S.codServico = I.codServico
+          WHERE S.codServico =".$_GET['ser1']." OR S.codServico=".$_GET['ser2']." OR S.codServico=".$_GET['ser3']." OR S.codServico=".$_GET['ser4'];
+    
     $resultado = mysqli_query($link,$sql);
     $rowcount = mysqli_num_rows($resultado);
     
     if($rowcount>0){
-        $row = mysqli_fetch_array($resultado,MYSQLI_ASSOC);  
-        echo json_encode($row); // retorna os dados para conferir o json
+        $row = mysqli_fetch_array($resultado,MYSQLI_ASSOC);
+        
+        foreach($row as $servico){
+            echo $servico;
+            // echo $servico['nomeservico']."   ".$servico['imgservico'];
+        }
+        //var_dump($row);
+        //die();
+        // echo json_encode($row); // retorna os dados para conferir o json
     }else{
         echo 0;
     }
